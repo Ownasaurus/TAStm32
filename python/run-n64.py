66771b67411f217acb6155f4e76156673723e336
TheMas3212 <mas3212@gmail.com>
2019-01-16 10:57:18 +1100

    Add receipt of Reset and Setup Confirmation

--- python/run-n64.py
+++ python/run-n64.py
@@ -8,7 +8,8 @@ run_id = b'A'
 
 def serial_read(ser, count):
     data = ser.read(count)
-    print('R: {}'.format(data))
+    if len(data) != 0:
+        print('R: {}'.format(data))
     return data
 
 def serial_write(ser, data):
@@ -16,12 +17,19 @@ def serial_write(ser, data):
     print('S: {}'.format(data))
     return r
 
+def serial_wait_for(ser, flag):
+    data = b''
+    while len(data) < len(flag):
+        data += serial_read(ser, len(flag)-len(data))
+    if data != flag:
+        raise RuntimeError()
+
 def main():
     if len(sys.argv) != 3:
         sys.stderr.write('Usage: ' + sys.argv[0] + ' <interface> <movie file>\n\n')
         sys.exit(0)
     try:
-        ser = serial.Serial(sys.argv[1], 2000000, timeout=1)
+        ser = serial.Serial(sys.argv[1], 115200, timeout=1)
     except SerialException:
         print ('ERROR: the specified interface (' + sys.argv[1] + ') is in use')
         sys.exit(0)
@@ -30,9 +38,12 @@ def main():
             data = f.read()
     except:
         print('ERROR: the specified file (' + sys.argv[2] + ') failed to open')
+        sys.exit(0)
     buffer = m64.read_input(data)
     serial_write(ser, b'R') # send RESET command
+    serial_wait_for(ser, b'\x01R')
     serial_write(ser, b'S' + run_id + b'M\x01')
+    serial_wait_for(ser, b'\x01S')
     for latch in buffer[:int_buffer]:
         serial_write(ser, run_id)
         serial_write(ser, latch)
