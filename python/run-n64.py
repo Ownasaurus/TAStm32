b46bc5eceedc5a25ef91489f54596b4131562028
TheMas3212 <mas3212@gmail.com>
2019-01-18 09:37:21 +1100

    Update python to react to error codes sent from device

--- python/run-n64.py
+++ python/run-n64.py
@@ -20,19 +20,19 @@ def serial_write(ser, data):
         print('S: {}'.format(data))
     return r
 
-def serial_wait_for(ser, flag):
+def serial_wait_for(ser, flag, err=None):
     data = b''
     while len(data) < len(flag):
         data += serial_read(ser, len(flag)-len(data))
     if data != flag:
-        raise RuntimeError()
+        raise RuntimeError(err)
 
 def main():
     if not len(sys.argv) in [3,4]:
         sys.stderr.write('Usage: ' + sys.argv[0] + ' <interface> <movie file>\n\n')
         sys.exit(0)
     try:
-        ser = serial.Serial(sys.argv[1], 115200, timeout=1)
+        ser = serial.Serial(sys.argv[1], 115200, timeout=0.1)
     except serial.SerialException:
         print ('ERROR: the specified interface (' + sys.argv[1] + ') is in use')
         sys.exit(0)
@@ -48,18 +48,42 @@ def main():
         blanks = 0
     buffer = m64.read_input(data)
     serial_write(ser, b'R') # send RESET command
-    serial_wait_for(ser, b'\x01R')
+    err = serial_read(2)
+    if err == b'\x01R':
+        pass
+    elif err == b'\xFF':
+        raise RuntimeError('Prefix not recognised')
+    else:
+        raise RuntimeError('Unknown error during reset')
     serial_write(ser, b'S' + run_id + b'M\x01')
-    serial_wait_for(ser, b'\x01S')
+    err = serial_read(2)
+    if err == b'\x01S':
+        pass
+    elif err == b'\xFF':
+        raise RuntimeError('Prefix not recognised')
+    elif err == b'\xFE':
+        raise RuntimeError('Run number not recognised')
+    elif err == b'\xFD':
+        raise RuntimeError('Number of controllers not recognised')
+    elif err == b'\xFC':
+        raise RuntimeError('Console Type not recognised')
+    else:
+        raise RuntimeError('Unknown error during run setup')
     for blank in range(blanks):
         data = run_id + b'\x00\x00\x00\x00'
         serial_write(ser, data)
-        print('Sending Blank Frame: {}'.format(blank))
+        err = serial_read(1)
+        if err == b'\xB0':
+            break
+        print('Sending Blank Latch: {}'.format(blank))
     fn = 0
-    for latch in range(int_buffer):
+    for latch in range(int_buffer-blanks):
         data = run_id + buffer[fn]
         serial_write(ser, data)
-        print('Sending Frame: {}'.format(fn))
+        err = serial_read(1)
+        if err == b'\xB0':
+            break
+        print('Sending Latch: {}'.format(fn))
         fn += 1
     done = False
     print('Main Loop Start')
@@ -72,14 +96,17 @@ def main():
             if numBytes > 0:
                 c += serial_read(ser, numBytes)
                 if numBytes > int_buffer:
-                    print ("WARNING: High frame read detected: " + str(numBytes))
+                    print ("WARNING: High latch rate detected: " + str(numBytes))
             if DEBUG:
                 print('R: {}'.format(c))
             latches = c.count(run_id)
             for latch in range(latches):
                 data = run_id + buffer[fn]
                 serial_write(ser, data)
-                print('Sending Frame: {}'.format(fn))
+                err = serial_read(1)
+                if err == b'\xB0':
+                    break
+                print('Sending Latch: {}'.format(fn))
                 fn += 1
         except serial.SerialException:
             print('ERROR: Serial Exception caught!')
