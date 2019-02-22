223c6a7850ed218070bb686ac4a0476ec69a79b0
Justin S <Ownasaurus@gmail.com>
2019-02-21 20:59:46 -0500

    now syncs with Lord Tom run

--- Src/main.c
+++ Src/main.c
@@ -233,7 +233,7 @@ static void MX_TIM3_Init(void)
   htim3.Instance = TIM3;
   htim3.Init.Prescaler = 840-1;
   htim3.Init.CounterMode = TIM_COUNTERMODE_UP;
-  htim3.Init.Period = 800-1;
+  htim3.Init.Period = 500-1;
   htim3.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
   if (HAL_TIM_Base_Init(&htim3) != HAL_OK)
   {
