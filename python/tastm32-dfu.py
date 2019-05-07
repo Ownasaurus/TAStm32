d69d112f2d2039bcd6d056f8305212e739c91346
Justin S <Ownasaurus@gmail.com>
2019-05-07 19:05:21 -0400

    script to put device in DFU mode

---
+++ python/tastm32-dfu.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+import serial, time
+import serial_helper
+import argparse_helper
+import tastm32
+
+parser = argparse_helper.audio_parser()
+args = parser.parse_args()
+
+if args.serial == None:
+    dev = tastm32.TAStm32(serial_helper.select_serial_port())
+else:
+    dev = tastm32.TAStm32(args.serial)
+
+# connect to device
+ser = dev
+
+print("--- Sending command to jump to DFU mode")
+
+# reset to make sure there is no leftover data
+ser.write(b'\xDF')
+time.sleep(0.1)
