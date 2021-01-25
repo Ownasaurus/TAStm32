29607af31455d5cfe21061b05a4944d15a7c14fa
CompuCat <sirwinstoncat5@gmail.com>
2021-01-25 00:03:08 -0600

    WIP

--- Inc/info.h
+++ Inc/info.h
@@ -34,31 +34,79 @@ static const char* info_field_vals[8] =
   "nes,snes,n64,gcn,genesis"
 };
 
-void serial_write_varint(int n)
+static inline void serial_write_varint(int n)
 {
-  uint8_t* buf = malloc(1);
+  uint8_t buf;
   if(n<=127)
   {
     buf=(uint8_t) n;
-    serial_interface_output(&buf, 1);
+    serial_interface_output((uint8_t*)&buf, 1);
   }
   else
   {
     buf=(uint8_t)(n & 0x8f);
-    serial_interface_output(&buf, 1);
+    serial_interface_output((uint8_t*)&buf, 1);
     buf=(uint8_t)(n >> 7);
-    serial_interface_output(&buf, 1);
+    serial_interface_output((uint8_t*)&buf, 1);
   }
 }
 
-void serial_write_InfoBlob()
+uint8_t* outbuf;
+uint8_t* writeptr;
+#define BUFSIZE (writeptr-outbuf)
+
+static inline void buffer_write_varint(int n)
+{
+  if(n<=127)
+  {
+    realloc(outbuf, BUFSIZE+1);
+    *writeptr=(uint8_t) n;
+    writeptr++;
+  }
+  else
+  {
+    realloc(outbuf, BUFSIZE+2);
+    writeptr[0]=(uint8_t)(n & 0x8f);
+    writeptr[1]=(uint8_t)(n >> 7);
+    writeptr+=2;
+  }
+}
+
+static inline void serial_write_InfoBlob()
 {
   int names_array_bytes = 0;
   for(int i=0; i<ARRLEN(info_field_names); i++) names_array_bytes += strlen(info_field_names[i])+1;
   int vals_array_bytes = 0;
   for(int i=0; i<ARRLEN(info_field_vals); i++) vals_array_bytes += strlen(info_field_vals[i])+1;
 
-  serial_interface_output((uint8_t*)"I", 1);
+  outbuf=malloc(1);
+  writeptr=outbuf;
+  outbuf[0]="I";
+  writeptr++;
+
+
+  buffer_write_varint(ARRLEN(info_field_names));
+  buffer_write_varint(names_array_bytes);
+  //Write field names
+  for(int i=0; i< ARRLEN(info_field_names); i++)
+  {
+    realloc(outbuf, BUFSIZE+strlen(info_field_names[i])+1);
+    memcpy(info_field_names[i], writeptr, strlen(info_field_names[i])+1);
+    writeptr+=(strlen(info_field_names[i])+1);
+  }
+
+  buffer_write_varint(vals_array_bytes);
+  for(int i=0; i< ARRLEN(info_field_vals); i++)
+  {
+    realloc(outbuf, BUFSIZE+strlen(info_field_vals[i])+1);
+    memcpy(info_field_vals[i], writeptr, strlen(info_field_vals[i])+1);
+    writeptr+=(strlen(info_field_vals[i])+1);
+  }
+
+  serial_interface_output(outbuf, BUFSIZE);
+  free(outbuf);
+
+  /*serial_interface_output((uint8_t*)"I", 1);
   serial_write_varint(ARRLEN(info_field_names));
   serial_write_varint(names_array_bytes);
   //Write field names
@@ -68,6 +116,7 @@ void serial_write_InfoBlob()
   serial_write_varint(vals_array_bytes);
   for(int i=0; i< ARRLEN(info_field_vals); i++)
     serial_interface_output(info_field_vals[i], strlen(info_field_vals[i])+1);
+    */
 }
 
 
