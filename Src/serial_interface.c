15704bdbe45a4666ca9cea8845113a195ec3d601
CompuCat <12215288+compucat@users.noreply.github.com>
2021-01-27 22:26:44 -0600

    serial_interface: Consistently use uppercase hex constants for style (#43)

--- Src/serial_interface.c
+++ Src/serial_interface.c
@@ -41,7 +41,7 @@ void serial_interface_consume(uint8_t *buffer, uint32_t n)
 			case SERIAL_PREFIX:
 				switch(input)
 				{
-					case '\xaa': // ping
+					case '\xAA': // ping
 						serial_interface_output((uint8_t*)"\x55", 1); // pong
 						break;
 					case 'U': // set up latch train for a run
