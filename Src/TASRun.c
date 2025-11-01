d29b3cc048d527416a3b1b25c1dce8c1aed9400e
TheMas3212 <mas3212@gmail.com>
2025-11-02 10:48:51 +1100

    changed GPIOA mask to not affect the reset pin

--- Src/TASRun.c
+++ Src/TASRun.c
@@ -144,7 +144,7 @@ void ResetGPIO(void){
 
 	// all GPIO pins by default should be input, with a pullup to stop them floating
 	// 87FF/34FB/FFCF chosen to avoid messing with peripheral pins
-	SetupPin(GPIOA, 0x87FF, GPIO_MODE_INPUT, GPIO_PULLUP, GPIO_PIN_RESET);
+	SetupPin(GPIOA, 0x85FF, GPIO_MODE_INPUT, GPIO_PULLUP, GPIO_PIN_RESET);
 	SetupPin(GPIOB, 0x34FB, GPIO_MODE_INPUT, GPIO_PULLUP, GPIO_PIN_RESET);
 	SetupPin(GPIOC, 0xFFCF, GPIO_MODE_INPUT, GPIO_PULLUP, GPIO_PIN_RESET);
 
